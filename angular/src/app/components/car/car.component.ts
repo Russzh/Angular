1a30db3844003140f1c0d0a1638b8bef6f63f861
Ruslan Zhukouski <Ruslan_Zhukouski@epam.com>
2019-07-22 10:51:16 +0300

    work with forms, addition and removing options

--- angular/src/app/components/car/car.component.ts
+++ angular/src/app/components/car/car.component.ts
@@ -29,6 +29,20 @@ export class CarComponent implements OnInit {
    // this.test = true; //любой тип данных
   }
 
+  addOpt(option) {
+    this.options.unshift(option);
+    return false;
+  }
+
+  deleteOpt(option){
+    for(let i = 0; i < this.options.length; i++){
+      if(this.options[i] == option) {
+        this.options.splice(i, 1);
+        break; 
+      }
+    }
+  }
+
   carSelect(carName) {
     if(carName == 'bmw'){ 
             this.name = 'BMW';
