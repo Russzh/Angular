4b7b4774a58c83a39a3efb0da3d07f87e275189d
Ruslan Zhukouski <Ruslan_Zhukouski@epam.com>
2019-07-22 15:19:54 +0300

    Work with routing, new component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
-import { from } from 'rxjs';
+import { ContactsComponent } from './components/contacts/contacts.component';
+
+const appRotes: Routes = [
+  {path: '', component: CarComponent},
+  {path: 'about', component: ContactsComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarComponent
+    CarComponent,
+    ContactsComponent
   ],
+
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRotes)
   ],
   providers: [],
   bootstrap: [AppComponent]
