4204f9f61b21933d0c28c66af9e1693121400899
Ruslan Zhukouski <Ruslan_Zhukouski@epam.com>
2019-07-23 15:12:18 +0300

    new project, new component, @input

---
+++ ng2/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  cards = [
+    {name: 'WFM 1'},
+    {name: 'WFM 2'},
+    {name: 'WFM 3'},
+    {name: 'WFM 4'},
+    {name: 'WFM 5'}
+  ] 
+
+}
