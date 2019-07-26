604d355bf351b80f8c42b5ad9c8b7003c952e3b8
Ruslan Zhukouski <Ruslan_Zhukouski@epam.com>
2019-07-26 16:46:36 +0300

    Addition routing

--- home-money/src/app/app.module.ts
+++ home-money/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthModule } from './auth/auth.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AuthModule
+    AuthModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
